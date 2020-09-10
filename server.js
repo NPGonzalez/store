f1e46692150c0299cbf57029ad17400b585ba1db
NPGonzalez <naypili@gmail.com>
2020-09-10 03:46:07 -0300

    Se configura express

--- server.js
+++ server.js
@@ -3,17 +3,9 @@ const app = express();
 
 
 
-connection.connect(function(error) {
-    if (!!error) {
-        console.log('Error');
-    } else {
-        console.log('Connnected');
-    }
-});
-
-
 app.get('/', (req, res) => {
-    res.send();
+
+    res.send('HOLA');
 
 });
 
