107ecfdae960a288d4c16248961e7e217ff2c968
NPGonzalez <naypili@gmail.com>
2020-09-10 03:49:03 -0300

    Se establece conexión con la base de datos

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
-
+const mysql = require('mysql');
 
 
 app.get('/', (req, res) => {
+
+    const connection = mysql.createConnection({
+        host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
+        user: 'bsale_test',
+        password: 'bsale_test',
+        database: 'bsale_test'
+    });
+
+    connection.connect(function(error) {
+        if (!!error) {
+            console.log('Error');
+        } else {
+            console.log('Connnected');
+        }
+    });
+
 
     res.send('HOLA');
 
